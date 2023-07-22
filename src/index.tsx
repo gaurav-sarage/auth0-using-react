16d08e25fd1b50412148a5dc929bb1adf366a469
Gaurav <gauravsarage.1902@gmail.com>
2023-07-22 23:59:18 +0530

    added index.tsx file

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain="dev-5tf99p7c.us.auth0.com"
+      clientId="GBPB42qhMWCtvrwGmYxvm5cbHXU68nzG"
+      redirectUri={window.location.origin + '/react-auth0/profile'}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
