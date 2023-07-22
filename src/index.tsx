3138155b1dc5e7ae73363f732d962ca4e91fd79e
Gaurav <gauravsarage.1902@gmail.com>
2023-07-23 00:11:27 +0530

    added components in App file and dissolved the issues

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
-import App from './App';
+import App from './App.tsx';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
